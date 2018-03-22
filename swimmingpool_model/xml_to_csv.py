094b967e3c637f585dba1809712d5864c5bff5ca
Henrique <htogashi@Henrique.lan>
2018-03-22 20:26:51 +1100

    model

---
+++ swimmingpool_model/xml_to_csv.py
@@ -0,0 +1,40 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+
+def main():
+    image_path = os.path.join(os.getcwd(), 'annotations')
+    xml_df = xml_to_csv(image_path)
+    msk = np.random.rand(len(xml_df)) < 0.8
+    train = xml_df[msk]
+    test = xml_df[~msk]
+    train.to_csv('data/train_labels.csv', index=None)
+    test.to_csv('data/test_labels.csv', index=None)
+    print('Successfully converted xml to csv.')
+
+
+main()
